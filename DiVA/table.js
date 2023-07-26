26fe6c869a7ee3f85b3f7d0120e7afa44abf39e9
diva360 <135677392+diva-2023@users.noreply.github.com>
2023-07-26 11:15:51 -0400

    update static selections

--- DiVA/table.js
+++ DiVA/table.js
@@ -2,7 +2,14 @@
 var datasetVar = "dynamic";
 var sequenceVar = "none";
 var dataResultVar = "none";
-var showDropBool = false;
+var selectionType = "dynamic-menu";
+var showDropBool = false; // for dynamic and canonical
+var showStaticClass = false; // for static dropdown categories
+var showStaticInstance = false; // for static instance categories
+var showStaticPos = false; // for static position catgories
+var staticClass = "none";
+var staticInstance = "none";
+var staticPos = "none";
 
 // ----------------------------- Dataset Toggle ----------------------------- //
 var dynamic = document.getElementById("dynamic");
@@ -22,19 +29,47 @@ function datasetClick(id) {
             options[i].classList.remove("selected");
         }
     }
-    datasetVar = id;
 
-    showDropdown();
-    if (sequenceVar != "none") {
+    if (sequenceVar != "none") { // if the sequence is set, reset
         selectDropdownOption("none");
     }
+    if (staticClass != "none") { // if static sequence is set, reset
+        selectStaticClassOption("none");
+        selectStaticInstanceOption("none");
+        selectStaticPosOption("none");
+        showStaticClass = false;
+        showStaticInstance = false;
+        showStaticPos = false;
+    }
+
+    datasetVar = id;
+    changeDatasetSelection();
 
     changeCommand();
 }
 
-// ----------------------------- Selection Toggle ----------------------------- //
+function changeDatasetSelection() {
+    var options = document.getElementsByClassName("sequence-options");
 
-// handles the dropdown selection menu content
+    if (datasetVar == "dynamic") {
+        selectionType = "dynamic-menu";
+    } else if (datasetVar == "static") {
+        selectionType = "static-menu";
+    } else {
+        selectionType = "canonical-menu"
+    }
+
+    // shows the different drop down menus based on the dataset
+    for (var i = 0; i < options.length; i++) {
+        if (options[i].id == selectionType && !options[i].classList.contains("show-dropdown")) {
+            options[i].classList.add("show-dropdown");
+        } else if (options[i].id != selectionType && options[i].classList.contains("show-dropdown")) {
+            options[i].classList.remove("show-dropdown");
+        }
+    }
+}
+
+// ----------- Dynamic and Canonical ----------- //
 function clickDropdown() {
     if (showDropBool) {
         showDropBool = false;
@@ -45,20 +80,12 @@ function clickDropdown() {
 }
 
 function showDropdown() {
-    var options = document.getElementsByClassName("dropdown-content");
+    var option = document.getElementById(selectionType).getElementsByClassName("dropdown-content")[0];
 
-    for (var i = 0; i < options.length; i++) {
-        if (showDropBool) {
-            if ((options[i].id != datasetVar) && options[i].classList.contains("show")) {
-                options[i].classList.remove("show");
-            } else if ((options[i].id == datasetVar) && !options[i].classList.contains("show")) {
-                options[i].classList.toggle("show");
-            }
-        } else {
-            if (options[i].classList.contains("show")) {
-                options[i].classList.toggle("show");
-            }
-        }
+    if (showDropBool) {
+        option.classList.toggle("show");
+    } else {
+        option.classList.remove("show");
     }
 }
 
@@ -69,12 +96,176 @@ function selectDropdownOption(id) {
         showDropdown();
     }
 
-    var sequence = document.getElementById("sequence-button");
-    sequenceVar = id;
-    element = document.getElementById(id);
+    // gets the selected element
+    var elementSelection = "#" + selectionType + " #" + id;
+    element = document.querySelector(elementSelection);
 
     // sets the sequence dropdown header
-    var seqHeader = document.getElementById("sequence-header");
+    var sequenceSelection = "#" + selectionType + " .sequence-button";
+    var sequence = document.querySelector(sequenceSelection);
+    var headerSelection = "#" + selectionType + " .sequence-header";
+    var seqHeader = document.querySelector(headerSelection);
+    seqHeader.innerHTML = element.innerHTML;
+    if (!sequence.classList.contains("selected")) {
+        sequence.classList.add("selected");
+    }
+
+    // sets the sequence variable
+    sequenceVar = id;
+
+    if (id == "none") {
+        if (sequence.classList.contains("selected")) {
+            sequence.classList.remove("selected");
+        }
+
+        // unselection sequence also removes data/result options
+        var options = document.getElementsByClassName("data-results-option");
+        for (var i = 0; i < options.length; i++) {
+            if (options[i].classList.contains("selected")) {
+                options[i].classList.remove("selected");
+                dataResultVar = "none";
+            }
+        }
+    }
+
+    changeCommand();
+}
+
+// ----------- Static ----------- //
+var classInstances = new Map();
+classInstances.set("cabinet", 9);
+classInstances.set("car", 12);
+classInstances.set("chair", 4);
+classInstances.set("couch", 9);
+classInstances.set("fruit", 3);
+classInstances.set("keyboard", 3);
+classInstances.set("mouse", 5);
+classInstances.set("mug", 8);
+classInstances.set("plane", 9);
+classInstances.set("scene", 4);
+classInstances.set("table", 8);
+classInstances.set("utensil", 14);
+
+function clickStaticDropdown(type) {
+    if (type == "static-class") {
+        if (showStaticClass) {
+            showStaticClass = false;
+        } else {
+            showStaticClass = true;
+        }
+        showStaticInstance = false;
+        showStaticPos = false;
+    } else if (type == "static-instance") {
+        if (showStaticInstance) {
+            showStaticInstance = false;
+        } else {
+            showStaticInstance = true;
+        }
+        showStaticClass = false;
+        showStaticPos = false;
+    } else {
+        if (showStaticPos) {
+            showStaticPos = false;
+        } else {
+            showStaticPos = true;
+        }
+        showStaticClass = false;
+        showStaticInstance = false;
+    }
+
+    showStaticDropdown();
+}
+
+function showStaticDropdown() {
+    var classOption = document.getElementById("static-class");
+    var instanceOption = document.getElementById("static-instance");
+    var posOption = document.getElementById("static-pos");
+
+    if (showStaticClass) {
+        classOption.classList.toggle("show");
+    } else {
+        classOption.classList.remove("show");
+    }
+
+    if (showStaticInstance) {
+        instanceOption.classList.toggle("show");
+    } else {
+        instanceOption.classList.remove("show");
+    }
+
+    if (showStaticPos) {
+        posOption.classList.toggle("show");
+    } else {
+        posOption.classList.remove("show");
+    }
+}
+
+function setStaticInstances(id) {
+    var selector = "#static-menu #static-instance";
+    var dropdownOptions = document.querySelector(selector);
+    dropdownOptions.innerHTML = "";
+
+    if (id == "none") {
+        dropdownOptions.innerHTML += `<div class="dropdown-option" id="none" onclick="selectStaticDropdownOption(this.id, 'static-instance')">None</div>`;
+        selectStaticDropdownOption("none", "static-instance");
+    } else {
+        var numInstances = classInstances.get(id);
+
+        // sets the dropdown options
+        for (var i = 0; i <= numInstances; i++) {
+            var instanceStr = String(i).padStart(2, '0');
+            var id = "instance_" + instanceStr;
+            dropdownOptions.innerHTML += `<div class="dropdown-option" id=${id} onclick="selectStaticDropdownOption(this.id, 'static-instance')">${instanceStr}</div>`;
+        }
+
+        // sets the default option
+        selectStaticDropdownOption("instance_00", "static-instance");
+    }
+}
+
+function setStaticPos(id) {
+    var selector = "#static-menu #static-pos";
+    var dropdownOptions = document.querySelector(selector);
+    dropdownOptions.innerHTML = "";
+
+    if (id == "none") {
+        dropdownOptions.innerHTML += `<div class="dropdown-option" id="none" onclick="selectStaticDropdownOption(this.id, 'static-pos')">None</div>`;
+        selectStaticDropdownOption("none", "static-pos");
+    } else {
+        var posList;
+        if (id == "scene") {
+            posList = ["clean", "messy1", "messy2", "messy3", "messy4", "messy5"];
+        } else {
+            posList = ["canonical", "random"];
+        }
+
+        // sets the dropdown options
+        for (var i = 0; i < posList.length; i++) {
+            var instanceStr = posList[i];
+            dropdownOptions.innerHTML += `<div class="dropdown-option" id=${instanceStr} onclick="selectStaticDropdownOption(this.id, 'static-pos')">${instanceStr}</div>`
+        }
+
+        // sets the default option
+        if (id == "scene") {
+            selectStaticDropdownOption("clean", "static-pos");
+
+        } else {
+            selectStaticDropdownOption("canonical", "static-pos");
+        }
+    }
+}
+
+function selectStaticDropdownOption(id, dropdownMenu) {
+    // gets the selected element
+    var elementSelection = "#" + selectionType + " #" + id;
+    element = document.querySelector(elementSelection);
+
+    // sets the sequence dropdown headers
+    var sequenceButtonSelection = "." + dropdownMenu + " .sequence-button"
+    var sequence = document.querySelector(sequenceButtonSelection);
+    var headerSelection = sequenceButtonSelection + " .sequence-header";
+    var seqHeader = document.querySelector(headerSelection);
+    console.log(headerSelection);
     seqHeader.innerHTML = element.innerHTML;
     if (!sequence.classList.contains("selected")) {
         sequence.classList.add("selected");
@@ -95,7 +286,29 @@ function selectDropdownOption(id) {
         }
     }
 
+    if (dropdownMenu == "static-class") {
+        staticClass = id;
+        setStaticInstances(id);
+        setStaticPos(id);
+        showStaticClass = false;
+    } else if (dropdownMenu == "static-instance") {
+        staticInstance = id.slice(-2);
+        showStaticInstance = false;
+    } else {
+        staticPos = id;
+        showStaticPos = false;
+    }
+    setSequenceVar();
     changeCommand();
+    showStaticDropdown();
+}
+
+function setSequenceVar() {
+    if (staticClass == "none") {
+        sequenceVar = "none";
+    } else {
+        sequenceVar = staticClass + staticInstance + "_" + staticPos;
+    }
 }
 
 // ----------------------------- Data/Results Toggle ----------------------------- //
@@ -117,20 +330,17 @@ function dataResultClick(id) {
 
                 // ensures that the sequence option is also selected 
                 if (sequenceVar == "none") {
-                    if (datasetVar == "dynamic") {
+                    if (datasetVar == "dynamic") { // dynamic
                         selectDropdownOption("battery");
-                    } else if (datasetVar == "static") {
-                        selectDropdownOption("cabinet00_canonical");
-                    } else {
+                    } else if (datasetVar == "static") { // static
+                        selectStaticDropdownOption("cabinet", "static-class");
+                    } else { // canonical
                         selectDropdownOption("cabinet");
                     }
                 }
             } else { // if the option is selected
                 options[i].classList.remove("selected");
                 dataResultVar = "none";
-
-                // removes the sequence option
-                selectDropdownOption("none");
             }
         } else if (options[i].id != id && options[i].classList.contains("selected")) {
             options[i].classList.remove("selected");
