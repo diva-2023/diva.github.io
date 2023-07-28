60f28de6f9de8ce73ad44c91f4497b687b04b894
diva360 <135677392+diva-2023@users.noreply.github.com>
2023-07-28 15:01:33 -0400

    update font sizes etc

--- DiVA/table.js
+++ DiVA/table.js
@@ -385,3 +385,30 @@ function changeCommand() {
     command += " &lt;path-to-destination&gt; --recursive --no-sign-request";
     document.getElementById("command").innerHTML = command;
 }
+
+async function copyCommand() {
+    text = document.getElementById("command").innerText;
+    // Navigator clipboard api needs a secure context (https)
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(text);
+    } else {
+        // Use the 'out of viewport hidden text area' trick
+        const textArea = document.createElement("textarea");
+        textArea.value = text;
+
+        // Move textarea out of the viewport so it's not visible
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+
+        document.body.prepend(textArea);
+        textArea.select();
+
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    };
+}
